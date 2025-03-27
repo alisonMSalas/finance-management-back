cfc370046dc9feecf64010edf983c6ef4d4a45c9
Rafael Soriano <raansopro324@gmail.com>
2025-03-27 18:29:42 -0500

    Implement budget management functionality with CRUD operations

--- src/main/java/uta/ec/finance_manager/repository/BudgetRepository.java
+++ src/main/java/uta/ec/finance_manager/repository/BudgetRepository.java
@@ -3,5 +3,8 @@ package uta.ec.finance_manager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uta.ec.finance_manager.entity.Budget;
 
+import java.util.List;
+
 public interface BudgetRepository extends JpaRepository<Budget, Integer> {
+    List<Budget> findAllByUserId(Integer userId);
 }
--- src/main/java/uta/ec/finance_manager/service/BudgetService.java
+++ src/main/java/uta/ec/finance_manager/service/BudgetService.java
@@ -1,4 +1,16 @@
 package uta.ec.finance_manager.service;
 
+import uta.ec.finance_manager.dto.BudgetDto;
+
+import java.util.List;
+
 public interface BudgetService {
+
+    BudgetDto save(BudgetDto budgetDto);
+
+    List<BudgetDto> getAllByUserId(Integer userId);
+
+    BudgetDto update(BudgetDto budgetDto);
+
+    void delete(Integer budgetId);
 }
