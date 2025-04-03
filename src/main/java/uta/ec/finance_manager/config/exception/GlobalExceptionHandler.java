a48cccb888c598d355ae8dbb21cebc291a958d4e
Rafael Soriano <raansopro324@gmail.com>
2025-04-03 11:04:46 -0500

    Exception handler

--- src/main/java/uta/ec/finance_manager/config/exception/GlobalExceptionHandler.java
+++ src/main/java/uta/ec/finance_manager/config/exception/GlobalExceptionHandler.java
@@ -3,14 +3,17 @@ package uta.ec.finance_manager.config.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationErrors(MethodArgumentNotValidException ex) {
@@ -26,5 +29,17 @@ public class GlobalExceptionHandler {
         response.put("errors", errors);
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> handleResponseStatusException(ResponseStatusException ex) {
+        return ResponseEntity
+                .status(ex.getStatusCode())
+                .body(Map.of(
+                        "timestamp", java.time.LocalDateTime.now(),
+                        "status", ex.getStatusCode().value(),
+                        "error", ex.getStatusCode(),
+                        "message", Objects.requireNonNull(ex.getReason())
+                ));
     }
 }
