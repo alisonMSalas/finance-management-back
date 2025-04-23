31681c6fca2d6cd1cd510ff94c1b493ee227fd91
Rafael Soriano <raansopro324@gmail.com>
2025-04-22 21:18:34 -0500

    Date format on DTO

--- src/main/java/uta/ec/finance_manager/dto/TransactionDto.java
+++ src/main/java/uta/ec/finance_manager/dto/TransactionDto.java
@@ -1,5 +1,6 @@
 package uta.ec.finance_manager.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,16 +12,23 @@ import java.util.Date;
 @Setter
 public class TransactionDto {
     private Integer id;
+
     @NotNull(message = "La cantidad es obligatoria")
     private Double amount;
+
     @NotNull(message = "El tipo es obligatorio")
     private String type;
+
     @NotNull(message = "La fecha es obligatoria")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "America/Guayaquil")
     private Date date;
+
     @NotNull(message = "La cuenta es obligatoria")
     private Integer accountId;
+
     @NotNull(message = "La categoria es obligatoria")
     private TransactionCategory category;
+
     private Integer userId;
     private String description;
 }
